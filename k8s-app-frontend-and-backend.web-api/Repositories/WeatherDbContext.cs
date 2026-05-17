63a29f78180e60886dfbc952783d3e7860bc5eb4
Stepan Lehkyi <stepan.lehkyi@hotmail.com>
2026-05-17 23:40:36 +0300

    Homework for lesson 7 is ready

---
+++ k8s-app-frontend-and-backend.web-api/Repositories/WeatherDbContext.cs
@@ -0,0 +1,11 @@
+﻿using k8s_app_frontend_and_backend.web_api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace k8s_app_frontend_and_backend.web_api.Repositories
+{
+  public class WeatherDbContext : DbContext
+  {
+    public WeatherDbContext(DbContextOptions<WeatherDbContext> options) : base(options) { }
+    public DbSet<WeatherForecast> WeatherForecasts { get; set; }
+  }
+}
